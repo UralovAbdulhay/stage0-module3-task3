ff2035ff3405676b95b8190344def93fb500a26c
Abdulhay <abdulhayuralov8185603@gmail.com>
2022-07-23 17:09:29 +0500

    fix bugs

--- src/main/java/lang/print/gaps/task3/ReassigningValues.java
+++ src/main/java/lang/print/gaps/task3/ReassigningValues.java
@@ -6,6 +6,7 @@ public class ReassigningValues {
         int second = 10;
         int third = 100;
 
+
         System.out.println(first);
         System.out.println(second);
         System.out.println(third);
